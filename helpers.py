fe16a40a80e1b014fb71b9981316484d748b241f
Braian Villasanti <braian.villasanti2@gmail.com>
2019-05-31 00:57:32 -0300

    Commit inicial

---
+++ helpers.py
@@ -0,0 +1,158 @@
+import random
+import statistics
+import webbrowser
+import matplotlib.pyplot as plt
+
+# Toma una lista "f" fitness, y devuelve un índice de un elemento de esa
+# lista usando el método de la ruleta.
+def ruleta(f):
+    numero = random.uniform(0,1)
+    fitness_coincidente = 0.0
+    for i in range(10):
+        if i >= 9:
+            return i
+        if numero > fitness_coincidente:
+            fitness_coincidente += f[i]
+        else:
+            return i
+
+# Completa los ceros por delante de una lista argumento para que tenga 30 dígitos
+def completar_ceros(b):
+    for i in range(30 - len(b)):
+        b.insert(0, '0')
+    return b
+
+# Hay una probabilidad "prob" de que se haga el crossover entre "a" y "b" en el punto "x" en la población "p"
+def crossover(p, a, b, x, prob):
+    if random.randint(0, 100) < prob*100:
+        # pdb.set_trace()
+        p.append(a[x:] + b[:x])
+        p.append(b[x:] + a[:x])
+        try:
+            p.remove(a)
+            p.remove(b)
+        except ValueError:
+            pass
+
+# Hay una probabilidad "prob" de que exista una mutación en un bit aleatorio del cromosoma
+def mutar(cromosoma, prob):
+    if random.randint(0, 100) < prob*100:
+        bit_cambiado = random.randint(0,29)
+        cromosoma[bit_cambiado] = str(abs(int(cromosoma[bit_cambiado]) - 1))
+
+# Crea un archivo HTML que muestra la información que se pide.
+def mostrar_info(cromosoma_final, maximos, minimos, promedios, prob_cross, prob_mut):
+    f = open('resultados.html', 'w')
+
+    html_inicial = """<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Trabajo Práctico N°1</title>
+            <link rel="stylesheet" type="text/css" href="styles.css">
+        </head>
+        <body>
+            <div>
+                <h1>Trabajo Práctico N°1</h1>
+                <h2>Algoritmos Genéticos</h2>
+                <p>Rafael Verde</p>
+                <p>Braian Villasanti</p>"""
+
+    # Muestra el cromosoma máximo final.
+    cromosoma_maximo = """<p><b>Cromosoma máximo: </b><div class="monoespaciado">%s</div></p>""" % cromosoma_final
+
+    #Muestra los valores de las probabilidades
+    valores = """<p><b>Probabilidad de crossover: </b>%d&#37 <b>Probabilidad de mutacion: </b>%d&#37 </p>
+    <h1>Tablas de valores</h1>""" %(prob_cross*100, prob_mut*100)
+
+    # Muestra el valor máximo, mínimo, y promedio de cada población.
+    header_tabla = """
+    <table>
+        <tr>
+            <th>Generación</th>
+            <th>Máximos</th>
+            <th>Mínimos</th>
+            <th>Promedios</th>
+        </tr>
+        """
+    
+    f.write(html_inicial)
+    f.write(cromosoma_maximo)
+    f.write(valores)
+    f.write(header_tabla)
+
+    for i in range(200):
+        tabla = """<tr>
+            <td>%d</td>
+            <td>%f</td>
+            <td>%f</td>
+            <td>%f</td>
+        </tr>""" %(i + 1, maximos[i], minimos[i], promedios[i])
+        f.write(tabla)
+
+    # Muestra tablas de máximos, mínimos, y promedios para 20, 100, y 200 corridas.
+    tabla2 = """</table>
+    <h1>Valores para las 20, 100, y 200 corridas</h1>
+    <table>
+        <tr>
+            <th>Generación</th>
+            <th>Máximos</th>
+            <th>Mínimos</th>
+            <th>Promedios</th>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>%f</td>
+            <td>%f</td>
+            <td>%f</td>
+        </tr>
+        <tr>
+            <td>100</td>
+            <td>%f</td>
+            <td>%f</td>
+            <td>%f</td>
+        </tr>
+        <tr>
+            <td>200</td>
+            <td>%f</td>
+            <td>%f</td>
+            <td>%f</td>
+        </tr>
+    </table>""" % (maximos[19], minimos[19], promedios[19], maximos[99], minimos[99], promedios[99], maximos[199], minimos[199], promedios[199])
+
+    plt.plot(range(200), maximos)
+    plt.xlim(0, 200)
+    plt.ylim(0, 1)
+    plt.autoscale(False)
+    plt.savefig('graficos/maximos.svg', bbox_inches='tight')
+    plt.clf()
+    
+
+    plt.plot(range(200), minimos)
+    plt.xlim(0, 200)
+    plt.ylim(0, 1)
+    plt.autoscale(False)
+    plt.savefig('graficos/minimos.svg', bbox_inches='tight')
+    plt.clf()
+
+    plt.plot(range(200), promedios)
+    plt.xlim(0, 200)
+    plt.ylim(0, 1)
+    plt.autoscale(False)
+    plt.savefig('graficos/promedios.svg', bbox_inches='tight')
+
+    html_final = """
+                <h1>Máximos<h1>
+                <img src="graficos/maximos.svg">
+                <h1>Mínimos<h1>
+                <img src="graficos/minimos.svg">
+                <h1>Promedios<h1>
+                <img src="graficos/promedios.svg">
+            </div>
+        </body>
+    </html>"""
+
+    f.write(tabla2)
+    f.write(html_final)
+    f.close()
+
+    webbrowser.open_new_tab('resultados.html')
---
+++ tp1.py
@@ -0,0 +1,92 @@
+## TRABAJO PRÁCTICO 1
+## Braian Villasanti, Rafael Verde
+
+import random
+from helpers import ruleta, completar_ceros, crossover, mutar, mostrar_info
+import statistics
+import argparse
+
+parser = argparse.ArgumentParser()
+poblacion = [] # Población en números enteros
+pob_bin = [] # Población en números binarios
+f_obj = [] # Resultados de la función objetivo para cada cromosoma
+fitness = [] # Resultados de la función fitness
+prob_cross = 0.75
+prob_mut = 0.05
+x = []
+x_minimos = []
+x_promedios= []
+x_maximos = []
+n = 10 # Tamaño de la población
+
+# Creación de argumentos para los comandos de cmd
+parser.add_argument("-c", "--crossover", help="Cambia el valor de la probabilidad de crossover", type=int)
+parser.add_argument("-m", "--mutacion", help="Cambia el valor de la probabilidad de mutación", type=int)
+args = parser.parse_args()
+
+#Verfica si se llamó a la flag y si esta cumple con los requisitos; si no cumple termina el programa
+if args.crossover:
+	if args.crossover <= 100 and args.crossover >= 0:
+		prob_cross = args.crossover/100
+	else:
+		print("! El nro de crossover tiene que ser entre 0 y 100")
+		exit()
+
+if args.mutacion:
+	if args.mutacion <= 100 and args.mutacion >= 0:
+		prob_mut = args.mutacion/100
+	else:
+		print("! El nro de mutacion tiene que ser entre 0 y 100")
+		exit()
+
+# Genera población inicial y la guarda
+for i in range(n):
+    x = random.randint(0, (2**30) - 1)
+    poblacion.append(x) # como números decimales
+    pob_bin.append(completar_ceros((list(str(bin(x))))[2:])) # y como números binarios de 30 dígitos.
+    f_obj.append((x/(2**30 - 1))**2) # Llena la tabla de función objetivo.
+
+# Genera resultados de función fitness.
+for i in range(n):
+    fitness.append(f_obj[i] / sum(f_obj))
+
+# Bucle de 200 iteraciones para cada iteración del algoritmo.
+for generacion in range(200):
+    resultado_ruleta = [] # Lista que guarda los padres que resultarán de la selección.
+    for i in range(n):
+        resultado_ruleta.append(pob_bin[ruleta(fitness)])
+ 
+    # Crossover
+    for i in range(0, 9, 2):
+        padre = resultado_ruleta[i]
+        madre = resultado_ruleta[i + 1]
+        punto_cross = random.randint(0,28)
+        crossover(pob_bin, padre, madre, punto_cross, prob_cross)
+
+    # Mutación
+    for i in range(n):
+        mutar(pob_bin[i], prob_mut)
+    
+    x_maximos.append(max(f_obj))
+    x_minimos.append(min(f_obj))
+    x_promedios.append(statistics.mean(f_obj))
+
+    # Resetear todos los datos menos los de la población binaria
+    poblacion = []
+    f_obj = []
+    fitness = []
+    
+    # Generar la nueva población en números enteros
+    for i in range(n):
+        poblacion.append(int(''.join(pob_bin[i]), 2))
+
+    # Genera resultados de la función objetivo
+    for i in range(n):
+        f_obj.append((poblacion[i]/(2**30 - 1))**2)
+
+    # Genera resultados de función fitness.
+    for i in range(n):
+        fitness.append(f_obj[i] / sum(f_obj))
+
+# Mostrar todo en un archivo HTML
+mostrar_info(''.join(max(pob_bin)), x_maximos, x_minimos, x_promedios, prob_cross, prob_mut)
