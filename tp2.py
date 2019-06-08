250f2b0cc95810504ab3dd382337f0a667d9532e
Braian Villasanti <braian.villasanti2@gmail.com>
2019-06-08 11:34:42 -0300

    Primer commit del trabajo práctico rehecho

---
+++ tp2.py
@@ -0,0 +1,10 @@
+# TODO:
+## Generar una población inicial.
+## Computar la función de evaluación de cada individuo.
+## Repetir 200 veces (producir nueva generación en cada iteración):
+#### Para cada miembro de la población (ciclo reproductivo):
+###### Selección: Seleccionar dos individuos de la anterior generación para el cruce.
+###### Crossover: Cruzar con cierta probabilidad los dos individuos obteniendo dos descendientes.
+###### Mutación: Mutar los dos descendientes con cierta probabilidad.
+###### Calcular la función evaluación de los dos descendientes mutados.
+###### Insertar los dos descendientes mutados en la nueva generación.
